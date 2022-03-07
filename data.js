72e41b9bb4cfb63729cc185e845faa78fa8af700
MIDU-SER <98685114+MIDU-SER@users.noreply.github.com>
2022-03-07 22:58:09 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,20 +1,20 @@
 module.exports = {
 
-mongoLink:'Link Here',// Database Link Of MongoDB
+mongoLink:'mongodb+srv://midlaj:midlaj@cluster0.x2tdj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',// Database Link Of MongoDB
 
 apikey:'Key Here',// Coinbase API Key
 
 secretkey:'Key ',// Coinbase 2nd Key
 
-bot_token:'Token Here',// Bot Token
+bot_token:'5108445196:AAE2bwj0-TzzQlSokagXHHdIP4dgzXE5p2Q',// Bot Token
 
-bot_name:'Name Here',// Bot Username Without @
+bot_name:'MjNodeBot',// Bot Username Without @
 
-bot_admin:11111111,// Admin Telegram Id
+bot_admin:1316079296,// Admin Telegram Id
 
-channelsList : ['@Cha'],// Channels List
+channelsList : ['@MjBotCheck'],// Channels List
 
-channelscheck : ['@Cha'],// Check Channels List
+channelscheck : ['@MjBotCheck'],// Check Channels List
 
 reffer_bonus:0.01,// Refferal Bonus Amount
 
@@ -24,6 +24,6 @@ daily_bonus:0.01,// Daily Bonus Amount
 
 currency:'DOGE', // Bot Currency
 
-payment_channel:'@Cha'// Payment Channel Username
+payment_channel:'@MjBotCheck'// Payment Channel Username
 
 }
